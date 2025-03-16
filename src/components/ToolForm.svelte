11112a4b6ded44d44896f1bb0e7f8add766db308
gzuuus <gzuuus@protonmail.com>
2025-03-16 23:09:59 +0100

    feat: dark light

--- src/components/ToolForm.svelte
+++ src/components/ToolForm.svelte
@@ -10,12 +10,6 @@
 
 	export let tool: { name: string; inputSchema: any };
 	let uiSchema: UiSchemaRoot = {
-		'ui:options': {
-			help: 'Help text',
-			input: {
-				placeholder: 'placeholder'
-			}
-		},
 		submitButton: {
 			'ui:options': {
 				title: 'Execute',
@@ -23,6 +17,11 @@
 					class:
 						'w-fit px-4 py-2 bg-primary text-background rounded-md hover:bg-primary/80 disabled:opacity-50 disabled:cursor-not-allowed'
 				}
+			}
+		},
+		'ui:globalOptions': {
+			input: {
+				class: 'bg-background/5'
 			}
 		}
 	};

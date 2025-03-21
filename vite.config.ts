22f94f23c384ff59c3c457f0a98d35ffebd39768
gzuuus <gzuuus@protonmail.com>
2025-03-21 11:14:57 +0100

    chore: vite options

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,19 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	server: {
+		host: true,
+		allowedHosts: ['dvmcp.fun']
+	},
+	build: {
+		sourcemap: false,
+		rollupOptions: {
+			output: {
+				manualChunks: {
+					vendor: ['svelte']
+				}
+			}
+		}
+	}
 });
